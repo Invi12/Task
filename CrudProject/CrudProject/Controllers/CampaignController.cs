47c1cdf5892081a6612c26bf3051fce41e2607b6
Invi12 <invi12@wp.pl>
2022-03-15 22:18:34 +0100

    Addin Controler and View
    Created database template and added single controler and view

---
+++ CrudProject/CrudProject/Controllers/CampaignController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrudProject.Models;
+
+namespace CrudProject.Controllers
+{
+    public class CampaignController : Controller
+    {
+        // GET: Campaign
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Campaigns> empList = db.Campaigns.ToList<Campaigns>();
+                return Json(new { date = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
